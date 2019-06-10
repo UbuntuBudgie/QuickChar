00ba3baa7f5b8eef9b95441a3308ae83714ef7e6
Jacob-Vlijm <vlijm@planet.nl>
2019-06-10 12:50:08 +0200

    Fixed browsing issue after latest commit, added mudule to paste chars, saving install of the whole pyautogui package.

---
+++ charpaste.py
@@ -0,0 +1,18 @@
+import os
+from Xlib.display import Display
+from Xlib import X
+from Xlib.ext.xtest import fake_input
+import Xlib.XK
+
+def paste_char(*args, **kwargs):
+    for c in args:
+        fake_input(_display, X.KeyPress, _to_keysim(c))
+        _display.sync()
+    for c in reversed(args):
+        fake_input(_display, X.KeyRelease, _to_keysim(c))
+        _display.sync()
+
+def _to_keysim(gotkey):
+    return _display.keysym_to_keycode(Xlib.XK.string_to_keysym(gotkey))
+
+_display = Display(os.environ['DISPLAY'])
