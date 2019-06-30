119eba5dbfe4132e7f9081e304a22e9330305660
David Mohammed <fossfreedom@ubuntu.com>
2019-06-30 10:58:18 +0100

    More pep8 fixes

--- data/meson_post_install.py
+++ data/meson_post_install.py
@@ -11,9 +11,8 @@ pkgdatadir = sys.argv[3]
 application_id = sys.argv[4]
 
 if not os.path.exists(bindir):
-  os.makedirs(bindir)
+    os.makedirs(bindir)
 
 src = os.path.join(pkgdatadir, application_id)
 dest = os.path.join(bindir, 'quickchar')
 subprocess.call(['ln', '-s', '-f', src, dest])
-
